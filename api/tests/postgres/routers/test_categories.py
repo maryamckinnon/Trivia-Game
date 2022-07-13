50b7d1321fd2e32b9ae4e6f6448921a547d9c765
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-07-12 19:34:01 -0700

    Wrote 2 additional tests

--- api/tests/postgres/routers/test_categories.py
+++ api/tests/postgres/routers/test_categories.py
@@ -15,6 +15,21 @@ class NormalCategoryQueries:
         return [id, "OUR CATEGORY", True]
 
 
+# class CreateCategoryQueries:
+#     def create_category(self, title):
+#         return [id, title, canon]
+
+
+class DeleteCategoryQueries:
+    def delete_category(self, id):
+        return {"result": True}
+
+
+class UpdateCategoryQueries:
+    def update_category(self, id, title):
+        return [id, title, False]
+
+
 def test_get_category_returns_404():
     app.dependency_overrides[CategoryQueries] = EmptyCategoryQueries
     response = client.get("api/postgres/categories/1")
@@ -30,4 +45,36 @@ def test_get_category_returns_200():
     assert d["id"] == 1
     assert d["title"] == "OUR CATEGORY"
     assert d["canon"] == True
+    app.dependency_overrides = {}
+
+
+# def test_update_category_returns_200():
+#     app.dependency_overrides[CategoryQueries] = UpdateCategoryQueries
+#     response = client.put("/api/postgres/categories/1")
+#     assert response.status_code == 200
+#     app.dependency_overrides = {}
+
+
+# def test_update_category_returns_404():
+#     app.dependency_overrides[CategoryQueries] = UpdateCategoryQueries
+#     response = client.put("/api/postgres/categories/1")
+#     assert response.status_code == 404
+#     app.dependency_overrides = {}
+    
+
+def test_delete_category_returns_true():
+    app.dependency_overrides[CategoryQueries] = DeleteCategoryQueries
+    response = client.delete("/api/postgres/categories/1")
+    d = response.json()
+    assert response.status_code == 200
+    assert d["result"] == True
+    app.dependency_overrides = {}
+
+
+def test_delete_category_returns_false():
+    app.dependency_overrides[CategoryQueries] = EmptyCategoryQueries
+    response = client.delete("/api/postgres/categories/1")
+    d = response.json()
+    assert response.status_code == 200
+    assert d["result"] == False
     app.dependency_overrides = {}
